e32dc82cfe1e346d48f21f836f52d862737b03ea
GeovannaAS <geovannavanna55@gmail.com>
2026-09-05 10:23:25 -0300

    feat:altera valores de banco de dados

--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -1,5 +1,11 @@
-export const tasks = [{
-    "id" : 1,
-    "title" : "Estudar rotas no express",
-    "done" : false
-}];
+import { TaskPriority, TaskStatus } from "../models/task.model";
+
+export const tasks = [  {
+    id: 1,
+    title: "Estudar TypeScript",
+    description: "Revisar interfaces e enums",
+    priority: TaskPriority.HIGH,
+    status: TaskStatus.PENDING,
+    createdAt: new Date(),
+  },
+];
